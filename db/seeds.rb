aa49a7f7581aed18a51e772f505aa0ab2eccb1b9
derickcastillo38 <9derick@gmail.com>
2018-12-05 09:40:12 -0500

    seeded database with 3 of my fav locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!([
+{ "name": "Brooklyn Bridge", "latitude": "40.7061","longitude": "73.9969"},
+{ "name": "UnionCity Reservoir", "latitude": "40.7649", "longitude": "74.0308"},
+{ "name": "Juniors Cheesecake", "latitude": "40.7583", "longitude": "-73.9863"}
+])
